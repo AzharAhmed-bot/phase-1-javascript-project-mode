314dd675641e4f2b6dab4b3f53363765c2e012b9
AzharTakoy <azharahmedtakoy4444@gmail.com>
2023-04-04 15:00:16 +0300

    mouseoverout

--- script.js
+++ script.js
@@ -20,12 +20,14 @@ fetch('http://localhost:3000/comics')
         </div>
       `;
       container.appendChild(card); // add the card to the container
+   
     });
-  })
-  .catch(error => console.log(error)); 
+})
 
-  const submitgrps=document.querySelector("#connectionForm")
-submitgrps.addEventListener('submit',(e)=>{
- e.preventDefault();
- console.log("Submitted")})
+
+ const connectionForm=document.querySelector("#connectionForm")
+ const submitgrps=document.querySelector("#submit-groups")
+  submitgrps.addEventListener('mouseover',(e)=>{console.log(e.target.style.color= "white")})
+  submitgrps.addEventListener("mouseout",(e)=>{console.log(e.target.style.color= "black")})
+  
 })
