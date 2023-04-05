be8530f9a942d5f9c1038483fab90d5b651db8d2
AzharTakoy <azharahmedtakoy4444@gmail.com>
2023-04-05 13:48:45 +0300

    event listener

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded',()=>{
+    function fetchToDisplay(){
 fetch('http://localhost:3000/comics')
   .then(response => response.json()) // parse the JSON data
   .then(data => {
@@ -17,17 +18,95 @@ fetch('http://localhost:3000/comics')
           <p class="card-text">${comic.description}</p>
           <p class="card-text">Superhero: ${comic.superhero}</p>
           <p class="card-text">Price: ${comic.price}</p>
+          <p class="card-text">Available: ${comic.available} books </p>
         </div>
       `;
       container.appendChild(card); // add the card to the container
    
     });
 })
+    }
+fetchToDisplay()
 
-
+function searchToDisplay(){
  const connectionForm=document.querySelector("#connectionForm")
+ const input = document.querySelector('#form-groups');
+ const display = document.querySelector('#display-submit');
  const submitgrps=document.querySelector("#submit-groups")
+ 
   submitgrps.addEventListener('mouseover',(e)=>{console.log(e.target.style.color= "white")})
   submitgrps.addEventListener("mouseout",(e)=>{console.log(e.target.style.color= "black")})
+  connectionForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const title = input.value;
+    fetch('http://localhost:3000/comics')
+    .then((response) => response.json())
+    .then((data) => {
+        let filtering=data.find((comic) => comic.title === title)
+       //console.log(filtering)
+        if (filtering) {
+            //console.log(data)
+            
+          display.innerHTML = `
+            <div class="comic">
+              <h2>${filtering.title}</h2>
+              <img src="${filtering.image}" alt="${filtering.superhero}" />
+              <p>${filtering.description}</p>
+              <p>Price: $${filtering.price}</p>
+              <p>Available: ${filtering.available} books</p>
+           <form id=getting-comment> <input id="comment" type="text" placeholder="leave a comment for us about the book"${filtering.comment}/>
+             <input id="submit-comment" type="submit" value="leave comment"/> 
+             <form/>
+            <button id="buying">Buy Now</button>
+            </div>
+          `;
+            const buyingButton=document.querySelector("#buying")
+            buyingButton.addEventListener('click',()=>{
+                console.log("clicked")
+            })
+
+
+
+     //-----------------------------------------------------------------------------------------------------------------------------//
+          const commentito=document.querySelector("#comment")
+           const submitCmnt=document.querySelector("#getting-comment")
+        submitCmnt.addEventListener('submit',(e)=>{
+            e.preventDefault();
+            let feedBack=commentito.value;
+            
+          console.log(feedBack)
+          fetch(`http://localhost:3000/comics/${filtering.id}`,{
+        method:'PATCH',
+        headers:{
+            'Content-type':'application/json'
+        },
+        body:JSON.stringify({comments : feedBack})
+    })
+    .then(resp=>{resp.json()})
+    .then(data=>{
+         alert("Comment Updated")
+    })
+    
+commentito.value=''
+        })
+          input.value=''
+        } else {
+          display.innerHTML = '<h1 id="error">Comic not found.</h1>';
+        }
+      })
+     // .catch((error) => {
+       // alert("Oops! wrong information")
+     //}); 
+    })
   
-})
+}
+searchToDisplay()
+  
+
+    
+
+
+
+});
+
+    
